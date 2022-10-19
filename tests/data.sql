1408c9252d71cd2f11430c14b362dbc50bab9bf0
MartiniThomas <thomas.martini098@gmail.com>
2022-10-19 17:21:28 +0200

    for conftest.py

---
+++ tests/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Request (program       , query       , evidence       , option_1       , option_2       , nSamples       , blocks       , upper       , errors) 
+             VALUES ("test_program", "test_query", "test_evidence", "test_option_1", "test_option_2", "test_nSamples", "test_blocks", "test_upper", "test_errors")
