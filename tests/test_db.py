70eeae5346828138aa99aa6074269dc15f767381
MartiniThomas <thomas.martini098@gmail.com>
2022-10-19 17:20:37 +0200

    added db communication tests

---
+++ tests/test_db.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+import pytest
+from flaskr.db import get_db
+
+"""
+GIVEN database
+WHEN we established a connection to it with "get_db"
+THEN check the connection end after calling "app_context()"
+"""
+def test_get_close_db(app):
+    with app.app_context():
+        db = get_db()
+        assert db is get_db()
+
+    with pytest.raises(sqlite3.ProgrammingError) as e:
+        db.execute('SELECT 1')
+
+    assert 'closed' in str(e.value)
+
+"""
+GIVEN 
+WHEN 
+THEN ????????????????????????
+"""
+def test_init_db_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+
+    def fake_init_db():
+        Recorder.called = True
+
+    monkeypatch.setattr('flaskr.db.init_db', fake_init_db)
+    result = runner.invoke(args=['init-db'])
+    assert 'Initialized' in result.output
+    assert Recorder.called
